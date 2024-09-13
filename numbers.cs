955c7472afa74ba70a93e535f3cbead52643f01d
DanielaOdhis <daniela.odhiambo@students.jkuat.ac.ke>
2024-09-13 19:19:27 +0300

    more string syntaxes

--- numbers.cs
+++ numbers.cs
@@ -10,21 +10,10 @@ namespace learningcsharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Friends!");
-            Console.WriteLine("Y'all I can code in c# now!");
-
-            string name = "Daniela";
-            Console.WriteLine("My name is " + name);
-            Console.WriteLine($"The name {name} has {name.Length} letters");
-
-            string age = "21";
-            Console.WriteLine($"I am  {age} years old");
-
             int a = 4;
             int b = 3;
             int c = a + b;
             Console.WriteLine(c);
-
 
             Console.ReadKey();
         }
