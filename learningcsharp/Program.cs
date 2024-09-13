e6a1613adab3281db59876ebe043997c50283650
DanielaOdhis <daniela.odhiambo@students.jkuat.ac.ke>
2024-09-13 18:30:58 +0300

    Add project files.

---
+++ learningcsharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learningcsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Friends!");
+            Console.WriteLine("Y'all I can code in c# now!");
+
+            string name = "Daniela";
+            Console.WriteLine("My name is " + name);
+            Console.ReadKey();
+        }
+    }
+}
